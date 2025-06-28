124eceffd7f292a39b85a7a6240d9296071badad
ViHugo03 <99813230+ViHugo03@users.noreply.github.com>
2025-06-28 02:02:16 -0300

    Atualizando dados de servidor

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  server: {
+    host: true, // permite acesso externo (IP da VM)
+    port: 5173, // ou outro que você quiser
+    strictPort: true,
+    open: true, // abre no navegador automaticamente
+  }
+})
